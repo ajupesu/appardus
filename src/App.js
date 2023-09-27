9dbfa1f1d6d5a27eea51ac7fbec023288ac87203
Aksel Talvoja <114922105+ajupesu@users.noreply.github.com>
2023-09-27 13:04:00 +0300

    27.09.23 - tunnitöö

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Hello from "./components/Hello"
 import Maths from "./components/Maths"
 import Info from "./components/Info"
 import New from "./components/New"
+import Toggle from "./components/Toggle"
+import { useState } from "react"
+import Show from "./components/Show"
 
 function App() {
+  const [show, setShow] = useState(true)
+
+  const toggleShow = () => setShow(previousShow => !previousShow)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <New />
-        <Info />
-        <Maths />
-      </header>
+    <div className="Container">
+      <h1>Hi</h1>
+      <New />
+      <Info />
+      <Maths />
+      <Toggle toggleShow={toggleShow} />
+      <Show show={show} />
     </div>
   )
 }
